531f24f87cb2f5964cb761cc82af043aba3329e5
codedcoder <codedcoderrr@gmail.com>
2024-10-30 16:46:12 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -12,18 +12,27 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: corsConfig,
     bodyParser: false,
+    bufferLogs: true,
   });
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.useGlobalFilters(new AllExceptionsFilter(app.get<Logger>(LOGGER)));
 
   const configService = app.get(ConfigService);
-  const port = configService.get<string>('port');
+  const port = configService.get<number>('port') || 3000;
 
   app.use(json({ limit: '100mb' }));
   app.use(urlencoded({ limit: '100mb', extended: true }));
 
   await app.listen(port);
-  console.log(`${process.env.MODE} app running on: ${await app.getUrl()}`);
+
+  const logger = app.get<Logger>(LOGGER);
+  logger.info(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
